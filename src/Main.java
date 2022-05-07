22ba47263fa6af37c8a638ec73dc3d8f6190c78a
JoJo <titichaya_love@hotmail.com>
2022-05-07 19:26:42 +0200

    [FIX]🔒️ change credentials to .env
    .env file is created for storing credentials

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,18 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import io.github.cdimascio.dotenv.Dotenv;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Dotenv dotenv = Dotenv.load();
 		
 		Connection connection = null;
-		String url = "jdbc:mariadb://localhost:3306/test";
-		String user = "root";
-		String password = "password";
+		String url = dotenv.get("URL");
+		String user = dotenv.get("USER");
+		String password = dotenv.get("ROOT_PASSWORD");
 		
 		try {
 			connection = DriverManager.getConnection(url,user,password);
