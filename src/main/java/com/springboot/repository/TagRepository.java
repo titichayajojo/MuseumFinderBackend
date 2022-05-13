bb78eee2a5407b4e81790c882eaf5ccf89328a94
JoJo <titichaya_love@hotmail.com>
2022-05-14 00:11:21 +0200

    [ADD]✨ Multiple Entry Points for different user's role. Implementing Tag model
    user, guest and admin have different entry points based on url. Tag model is also implemented

---
+++ src/main/java/com/springboot/repository/TagRepository.java
@@ -0,0 +1,13 @@
+package com.springboot.repository;
+
+import com.springboot.entity.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface TagRepository extends JpaRepository<Tag, Long> {
+    Optional<Tag> findByName(String name);
+    Optional<Tag> findById(Long id);
+
+    Boolean existsByName(String name);
+}
