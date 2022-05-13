6df78b8952fbe121343b549074e2c8fa3ed9a03b
JoJo <titichaya_love@hotmail.com>
2022-05-13 17:01:04 +0200

    [ADD]✨ User authentication Login and Register
    Implement User model, Login and Register.

---
+++ src/main/java/com/springboot/payload/SignUpDto.java
@@ -0,0 +1,17 @@
+package com.springboot.payload;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class SignUpDto {
+    private String firstName;
+    private String lastName;
+    private String dateOfBirth;
+    private String username;
+    private String email;
+    private String password;
+    private String role;
+    private int zipCode;
+}
