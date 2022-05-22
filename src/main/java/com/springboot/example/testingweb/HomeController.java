ab10e4d173488a821944ab888422883ab26778c6
JoJo <titichaya_love@hotmail.com>
2022-05-22 20:28:47 +0200

    [ADD]✨ user profile html, css
    added user profile ui and fetch user's data from the database

--- src/main/java/com/springboot/example/testingweb/HomeController.java
+++ src/main/java/com/springboot/example/testingweb/HomeController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 
-@Controller
+@RestController
 public class HomeController {
 
     @Autowired
