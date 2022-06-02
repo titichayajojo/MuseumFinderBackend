85fe8ef48b9a2c10659878b5cbc8207195bff4ad
JoJo <titichaya_love@hotmail.com>
2022-06-02 20:37:22 +0200

    [ADD]✨ exhibitions controller, repository and model

--- src/main/java/com/springboot/repository/ExhibitionRepository.java
+++ src/main/java/com/springboot/repository/ExhibitionRepository.java
@@ -1,13 +1,20 @@
 package com.springboot.repository;
 
 import com.springboot.entity.Exhibition;
+import com.springboot.entity.Museum;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.validation.Valid;
 import java.util.Optional;
 
 public interface ExhibitionRepository extends JpaRepository<Exhibition, Long> {
     Optional<Exhibition> findByName(String name);
     Optional<Exhibition> findById(Long id);
-
     Boolean existsByName(String name);
+
 }
